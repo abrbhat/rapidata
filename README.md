644fc191288dec99c28a61ac4cd4ad1af8a85d8b
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-09-23 17:31:31 +0530

    Initial commit with mysql config

---
+++ README.md
@@ -0,0 +1,21 @@
+RapiData-TermAPI
+================
+API Specification:
+Request URL: /datasets/find?list[:term1,term2...]
+Response: 
+<datasets>
+	<dataset>
+		<title>
+			Title
+		<title>
+		<link>
+			Link
+		</link>
+		<description>
+			Description
+		</description>
+	<dataset>
+		.
+		.
+		.
+</dataset>
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RapiDataTermapi::Application.config.secret_token = 'cc9722ae50145e5b89e3041af64a48586659a5f7ac733def791c2c5929e2d5461d5b1ba0aef20600a12628bf0f7ea0fe5b004490b74ea05076786063f875cca4'
