b41f38f1c0a021e318288edf9909eaef00118405
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-09-28 02:37:03 +0530

    Added JSON Parsing

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -94,9 +94,5 @@ class DatasetsController < ApplicationController
 
     #Select top 100 datasets according to frequency of terms
     @datasets=@datasets.sort_by {|key,value| value}.reverse.first(25)
-    
-    respond_to do |format|
-      format.xml
-    end
   end
 end
