9d51a7ca4c68dd66f8e685813028ffb28e72af2f
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-09-29 18:45:39 +0530

    modified:   app/assets/javascripts/rapidatajs.js

--- app/assets/javascripts/rapidatajs.js
+++ app/assets/javascripts/rapidatajs.js
@@ -1,10 +1,8 @@
  $(document).ready(function(){
-
-        var input = localStorage.getItem('myInput');
-        $('#search_box').val(input);
-        var timeoutReference;
+      var input = localStorage.getItem('myInput');
+      $('#search_box').val(input);
+      var timeoutReference;
       $('#search_box').keyup(function() {
-        
         if (timeoutReference) clearTimeout(timeoutReference);
         timeoutReference = setTimeout(function() {
           var term_val = send_ajax_call();
@@ -15,51 +13,31 @@
           var input_value = $('#search_box').val() +" "+ selected_tag;
           $('#search_box').val(input_value);
           var term_val = send_ajax_call();
-      });
-    
+      });    
 });
- function createRandomColor() { 
- 
+ function createRandomColor() {  
     var hex = '0123456789ABC'.split(''),  
-    color = '#', i; 
-     
+    color = '#', i;      
     for (i = 0; i < 6; i += 1) {  
         color = color + hex[Math.floor(Math.random() * 13)];  
-    }  
- 
+    } 
     return color;  
 }
-
 function setFontSize() {
-
-
     var maxFontSize = 27;
-    var fontSize = Math.floor(Math.random() * maxFontSize + 14) + 'px';
-    
+    var fontSize = Math.floor(Math.random() * maxFontSize + 14) + 'px';    
     return fontSize;
-
-   
-
-
 }
-
-
 function setOffsets() {
-
-  var offsets = {};
-  
+  var offsets = {};  
   var randTop = Math.floor(Math.random() * 10);
-  var randLeft = Math.floor(Math.random() * 10);
-  
+  var randLeft = Math.floor(Math.random() * 10);  
   var maxTop = Math.floor(Math.random() * randTop) + 'px';
   var maxLeft = Math.floor(Math.random() * randLeft) + 'px';
-
   offsets.top = maxTop;
-  offsets.left = maxLeft;
-   
+  offsets.left = maxLeft;   
   return offsets;    
-} 
-
+}
 function send_ajax_call(){
     var value = $('#search_box').val();
     $.ajax({
@@ -117,7 +95,7 @@ function send_ajax_call(){
                   $('#tag-cloud').html(terms);
                   j++;
               });
-              $('#tag-cloud li').each(function() {
+              $('#tag-cloud .tags_val').each(function() {
               var $a = $(this);
               var cssColor = createRandomColor();
               var cssFontSize = setFontSize();
@@ -127,8 +105,6 @@ function send_ajax_call(){
         error: function(){
                 alert("Oops!Something went wrong.");
         }
-
   });
-
-    return value;
+return value;
 }
