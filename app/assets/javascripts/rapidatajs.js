04199e8fcbb0580ae0eb5484c40d14175f5f3e19
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-09-29 21:26:35 +0530

    UI

--- app/assets/javascripts/rapidatajs.js
+++ app/assets/javascripts/rapidatajs.js
@@ -91,8 +91,8 @@ function send_ajax_call(){
                 var linkOffsets = setOffsets();
                 $tags_div.css({color: cssColor});
               }); 
-            alert($('.tags_val').first().css("height"));
-             $('#tag-cloud').css('height',(($('.tags_val').first().css("height")+10)*4));
+            alert($('.tags_val').first().css("height")+10px);
+             $('#tag-cloud').css('height',(($('.tags_val').first().css("height")+10px)*4));
 
         },
         error: function(){
