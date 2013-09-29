532a425a61bab339a6baa5dfcc1c3af013f0462a
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-09-29 16:22:23 +0530

    Added Seach Controller and removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ RapiDataTermapi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'search#index', :as => 'search'
 
   # See how all your routes lay out with "rake routes"
 
