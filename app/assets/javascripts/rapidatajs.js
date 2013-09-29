13e5faaf923975b0f5a3facb15f0f08d41f622e1
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-09-29 18:29:02 +0530

    modified:   app/assets/javascripts/rapidatajs.js

--- app/assets/javascripts/rapidatajs.js
+++ app/assets/javascripts/rapidatajs.js
@@ -7,14 +7,14 @@
         
         if (timeoutReference) clearTimeout(timeoutReference);
         timeoutReference = setTimeout(function() {
-          var term_val = send_azax_call();
+          var term_val = send_ajax_call();
         }, 500);
     });
       $('#tag-cloud').on('click', 'li', function () {
          var abcd = $(this).text();
           var input_value = $('#search_box').val() +" "+ abcd;
           $('#search_box').val(input_value);
-          var term_val = send_azax_call();
+          var term_val = send_ajax_call();
       });
     
 });
@@ -46,98 +46,81 @@ function setFontSize() {
 
 function setOffsets() {
 
-    var offsets = {};
-    
-    var randTop = Math.floor(Math.random() * 10);
-    var randLeft = Math.floor(Math.random() * 10);
-    
-    var maxTop = Math.floor(Math.random() * randTop) + 'px';
-    var maxLeft = Math.floor(Math.random() * randLeft) + 'px';
+  var offsets = {};
+  
+  var randTop = Math.floor(Math.random() * 10);
+  var randLeft = Math.floor(Math.random() * 10);
+  
+  var maxTop = Math.floor(Math.random() * randTop) + 'px';
+  var maxLeft = Math.floor(Math.random() * randLeft) + 'px';
 
-   offsets.top = maxTop;
-   offsets.left = maxLeft;
+  offsets.top = maxTop;
+  offsets.left = maxLeft;
    
-   return offsets;    
+  return offsets;    
 } 
 
-function send_azax_call(){
-
-        var value = $('#search_box').val();
+function send_ajax_call(){
+    var value = $('#search_box').val();
     $.ajax({
-         type: "GET",
-         url: "datasets/retrieve_from_terms.xml",
-         //url:"retrieve_from_terms.json",
-         dataType:"xml",
-         async:"true",
-         data:{"terms":value},
-        success: function (xml) {
-
-            // Parse the xml file and get data
-                 var result="";
-                 $("#search_result").html(result);
-                var i=1;
-                $(xml).find('dataset').each(function() {
-                 // Do something to each item here...
-                        var dataset_title = $(this).find('title').text();
-                        var dataset_description = $(this).find('description').text();
-                        var tags = "";
-                        $(this).find('term').each(function(){
-                            tags += $(this).text() +", ";
-
-                        });
-                        
-                         result += '<div class ="search_head">'+dataset_title+"</div>"+
-                             '<div class="search_desc">'+dataset_description+"</div>"+
-                             '<div class="tag">'+"Related Tags are: "+tags+"</div>"+
-                             '<div class= "download_links">'+
-                                                '<div class="row">'+
-                                            '<div class="col-md-4">'+
-                                            '</div>'+
-                                            '<div class="col-md-2">'+
-                                                '<button type="button" class="btn btn-info ">    Download in xml    </button>'+
-                                            '</div>'+
-                                            '<div class="col-md-2">'+
-                                                '<button type="button" class="btn btn-info ">    Download in excel    </button>'+
-
-                                            '</div>'+
-                                            '<div class="col-md-4">'+
-                                            '</div>'+
-                                            '</div>'+
-                                            '</div>';
-                                         
-                             $('#search_result').html(result);
-                          i++;
-                    });
-                        var terms ="";
-                        var j = 1;
-                    $(xml).find('list_term').each(function() {
-                        var term_value = $(this).text();
-                        terms += '<li class ="tags_val">'+ term_value+'</li>';
-
-                        if(j%7 == 0)
-                        {
-                            terms += '<br>';
-                        }
-                        $('#tag-cloud').html(terms);
-                        j++;
-
-                    });
-                    $('#tag-cloud li').each(function() {
-    
-            var $a = $(this);
-        var cssColor = createRandomColor();
-        var cssFontSize = setFontSize();
-        
-        var linkOffsets = setOffsets();
-        
-        
-        $a.css({color: cssColor, fontSize: cssFontSize, top: linkOffsets.top, left: linkOffsets.left});
-        
-    
-    });
-
-                    
-               
+      type: "GET",
+      url: "datasets/retrieve_from_terms.xml",
+      //url:"retrieve_from_terms.json",
+      dataType:"xml",
+      async:"true",
+      data:{"terms":value},
+      success: function (xml) {
+          // Parse the xml file and get data
+          var result="";
+          $("#search_result").html(result);
+          var i=1;
+          $(xml).find('dataset').each(function() {
+           // Do something to each item here...
+                var dataset_title = $(this).find('title').text();
+                var dataset_description = $(this).find('description').text();
+                var tags = "";
+                $(this).find('term').each(function(){
+                    tags += $(this).text() +", ";
+                });                  
+                result += '<div class ="search_head">'+dataset_title+"</div>"+
+                     '<div class="search_desc">'+dataset_description+"</div>"+
+                     '<div class="tag">'+"Related Tags are: "+tags+"</div>"+
+                     '<div class= "download_links">'+
+                        '<div class="row">'+
+                            '<div class="col-md-4">'+
+                            '</div>'+
+                            '<div class="col-md-2">'+
+                              '<button type="button" class="btn btn-info ">    Download in xml    </button>'+
+                            '</div>'+
+                            '<div class="col-md-2">'+
+                              '<button type="button" class="btn btn-info ">    Download in excel    </button>'+
+                            '</div>'+
+                            '<div class="col-md-4">'+
+                            '</div>'+
+                        '</div>'+
+                      '</div>';
+                                 
+                     $('#search_result').html(result);
+                  i++;
+              });
+              var terms ="";
+              var j = 1;
+              $(xml).find('list_term').each(function() {
+                  var term_value = $(this).text();
+                  terms += '<li class ="tags_val">'+ term_value+'</li>';
+                  if(j%7 == 0)
+                  {
+                      terms += '<br>';
+                  }
+                  $('#tag-cloud').html(terms);
+                  j++;
+              });
+              $('#tag-cloud li').each(function() {
+              var $a = $(this);
+              var cssColor = createRandomColor();
+              var cssFontSize = setFontSize();
+              var linkOffsets = setOffsets();
+              $a.css({color: cssColor, fontSize: cssFontSize, top: linkOffsets.top, left: linkOffsets.left});});
         },
         error: function(){
                 alert("Oops!Something went wrong.");
