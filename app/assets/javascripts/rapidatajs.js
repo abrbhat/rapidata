49b90ec9b8a515439aab773fe0dbdeadd38f11b6
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-10-01 02:18:31 +0530

    UI improved

--- app/assets/javascripts/rapidatajs.js
+++ app/assets/javascripts/rapidatajs.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
         delete_tag();
         var term_val = send_ajax_call();
       });     
-
+    hide_no_matching_tags_message();
     hide_loader();
 });
 
@@ -38,6 +38,8 @@ function delete_tag()
   }
 function hide_loader() { $('#ajax_loader').hide();}
 function show_loader() { $('#ajax_loader').show();}
+function hide_no_matching_tags_message() { $('#no_matching_tags').hide();}
+function show_no_matching_tags_message() { $('#no_matching_tags').show();}
 function send_ajax_call(){
     var search_text = $('#search_box').val();
     $.ajax({
@@ -60,58 +62,67 @@ function send_ajax_call(){
                 alert("Oops!Something went wrong.");
                 hide_loader();
         }
-  });
+   });
 return search_text;
 }
 function process_ajax_response(xml)
 {
-   var result="";
-          $("#search_result").html(result);
-          $(xml).find('dataset').each(function() {
-           // Constructing Search Listings
-                var dataset_title = $(this).find('title').text();
-                var dataset_description = $(this).find('description').text();
-                var link_to_dataset=  $(this).find('link').text();
-                var download_link = $(this).find('download_link').text();               
-                result += 
-                   '<div class="search_listing">'
-                +     '<a href="http://'+link_to_dataset+'"><div class ="search_head"><u>'+dataset_title+"</u></div></a>"
-                +     '<div class="search_desc">'+dataset_description+"</div>"
-                +     '<div class= "download_links">'
-                +        '<div class="row">'
-                +            '<div class="col-md-1 download_text">Download:</div>'
-                +            '<a href="http://'+download_link+'&format=xls"><div class="col-md-1">'
-                +              '<button type="button" class="btn btn-info btn-sm" >XLS</button>'
-                +            '</div></a>'
-                +            '<a href="http://'+download_link+'&format=xlsx"><div class="col-md-1">'
-                +              '<button type="button" class="btn btn-info btn-sm" >XLSX</button>'
-                +            '</div></a>'
-                +            '<a href="http://'+download_link+'&format=json"><div class="col-md-1">'
-                +              '<button type="button" class="btn btn-info btn-sm" >JSON</button>'
-                +            '</div></a>'
-                +            '<a href="http://'+download_link+'&format=jsonp"><div class="col-md-1">'
-                +              '<button type="button" class="btn btn-info btn-sm" >JSONP</button>'
-                +            '</div></a>'
-                +            '<a href="http://'+download_link+'&format=xml"><div class="col-md-1">'
-                +              '<button type="button" class="btn btn-info btn-sm" >XML</button>'
-                +            '</div></a>'                
-                +            '<div class="col-md-4"></div>'
-                +        '</div>'
-                +      '</div>'
-                + '</div>';
-                                 
-                     $('#search_result').html(result);
-              });
-              var terms ="";
-              // Constructing Tag Cloud
-              $(xml).find('list_term').each(function() {
-                  var term_value = $(this).text();
-                  terms += '<a href="#"><div class ="tags_val">'+ term_value+'</div></a>';
-                  $('#tag-cloud').html(terms);
-              });
-              $('#tag-cloud .tags_val').each(function() {
-                var $tags_div = $(this);
-                var cssColor = createRandomColor();
-                $tags_div.css({color: cssColor});
-              }); 
+  var result="";
+  $("#search_result").html(result);
+  $(xml).find('dataset').each(function() {
+      // Constructing Search Listings
+      var dataset_title = $(this).find('title').text();
+      var dataset_description = $(this).find('description').text();
+      var link_to_dataset=  $(this).find('link').text();
+      var download_link = $(this).find('download_link').text();               
+      result += 
+         '<div class="search_listing">'
+      +     '<a href="http://'+link_to_dataset+'"><div class ="search_head"><u>'+dataset_title+"</u></div></a>"
+      +     '<div class="search_desc">'+dataset_description+"</div>"
+      +     '<div class= "download_links">'
+      +        '<div class="row">'
+      +            '<div class="col-md-1 download_text">Download:</div>'
+      +            '<a href="http://'+download_link+'&format=xls"><div class="col-md-1">'
+      +              '<button type="button" class="btn btn-default btn-xs" >XLS</button>'
+      +            '</div></a>'
+      +            '<a href="http://'+download_link+'&format=xlsx"><div class="col-md-1">'
+      +              '<button type="button" class="btn btn-default btn-xs" >XLSX</button>'
+      +            '</div></a>'
+      +            '<a href="http://'+download_link+'&format=json"><div class="col-md-1">'
+      +              '<button type="button" class="btn btn-default btn-xs" >JSON</button>'
+      +            '</div></a>'
+      +            '<a href="http://'+download_link+'&format=jsonp"><div class="col-md-1">'
+      +              '<button type="button" class="btn btn-default btn-xs" >JSONP</button>'
+      +            '</div></a>'
+      +            '<a href="http://'+download_link+'&format=xml"><div class="col-md-1">'
+      +              '<button type="button" class="btn btn-default btn-xs" >XML</button>'
+      +            '</div></a>'                
+      +            '<div class="col-md-4"></div>'
+      +        '</div>'
+      +      '</div>'
+      + '</div>';
+      $('#search_result').html(result);
+        });
+      var terms ="";
+
+      // Constructing Tag Cloud
+      $(xml).find('list_term').each(function() {
+          var term_value = $(this).text();
+          terms += '<a href="#"><div class ="tags_val">'+ term_value+'</div></a>';
+          $('#tag-cloud').html(terms);
+      });
+
+      //Change colour only if terms are changing
+      if (terms) 
+      {        
+        $('#tag-cloud .tags_val').each(function() {
+          var $tags_div = $(this);
+          var cssColor = createRandomColor();
+          $tags_div.css({color: cssColor});
+        }); 
+      }    
+      else
+      {
+        show_no_matching_tags_message();
+      }  
 }
