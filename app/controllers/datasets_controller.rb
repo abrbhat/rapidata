d207821f5f765f0891fae127c8b0c4d1defc62f9
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-09-30 17:20:53 +0530

    Modified datasets controller and xml builder

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -2,21 +2,29 @@ class DatasetsController < ApplicationController
   def retrieve_from_terms
     headers['Access-Control-Allow-Origin'] = "*" 
     @datasets=Hash.new(0)
-    @terms=Hash.new(0)
-    requested_terms=params[:terms].split(/[ -,.:;+]/)
-    requested_terms.each do |requested_term|
+    @terms=Array.new()
+    @terms_related_to_requested_term=Hash.new
+
+    @requested_terms=params[:terms].split(/[ -,.:;+]/)
+    @requested_terms.each do |requested_term|
+      @terms_related_to_requested_term[requested_term]=Array.new()
       Term.find(:all, :conditions => ["lower(term) = ?", requested_term.downcase]).each do |term|
         term.term_items.each do |term_item|
           @datasets[term_item.dataset] += 1
           term_item.dataset.term_items.each do |ds_term|
-            @terms[ds_term.term.term] += 1
+            @terms_related_to_requested_term[requested_term] << ds_term.term.term
           end
         end
       end  
+      if(@terms.empty?)
+        @terms += @terms_related_to_requested_term[requested_term]
+      else
+        @terms = @terms & @terms_related_to_requested_term[requested_term]
+      end
     end
 
     #Select top 25 datasets according to frequency of terms
     @datasets=@datasets.sort_by {|key,value| value}.reverse.first(25)
-    @terms=@terms.sort_by {|key,value| value}.reverse.first(33)
+    @terms=@terms.first(33)
   end
 end
--- app/controllers/term_items_controller.rb
+++ app/controllers/term_items_controller.rb
@@ -1,9 +1,9 @@
-class TermItemsController < ApplicationController
+lass TermItemsController < ApplicationController
   def build
     Dataset.all.each do |dataset|
       tagger= EngTagger.new
 
-      text=[dataset.title,dataset.description,dataset.department,dataset.sector].join(' ')
+      text=[dataset.title,dataset.description,dataset.department].join(' ')
       tagged=tagger.add_tags(text.gsub(/(\.)/,'. '))
       nouns=tagger.get_nouns(tagged)
 
@@ -17,12 +17,26 @@ class TermItemsController < ApplicationController
           if(refined_noun =~ /^[a-z0-9]*$/)
             refined_noun.capitalize!
           end
-          if( (refined_noun =~ /^[1-9a-zA-Z]\w{1,}/) && not(refined_noun =~ /^[a-z]\w{1,4}/) )
+          if(first_character_is_not_zero(refined_noun) and word_is_not_an_acronym_and_not_less_than_5_character(refined_noun) and word_is_not_in_camelcase(refined_noun))
             term=Term.find_or_create_by_term(refined_noun)
             term_item=TermItem.find_or_create_by_term_id_and_dataset_id(term.id,dataset.id)
           end
         end
       end
     end        
-  end 
+  end
+  private 
+  def first_character_is_not_zero(string)
+    string =~ /^[1-9a-zA-Z]\w{1,}/
+  end  
+  def word_is_not_an_acronym_and_not_less_than_5_character(string)
+    return (  not(string =~ /^[A-Z][a-z0-9]{1,4}$/) or word_is_a_month(string)  )
+  end
+  def word_is_a_month(string)
+    string.in?(["June","July","May"])
+  end
+  def word_is_not_in_camelcase(string)
+    not(string =~ /^[A-Z][a-z0-9]+([A-Z][a-z0-9]*)+$/)
+  end
 end
+
