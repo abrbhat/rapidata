e46bcf9685aadd3181385d42446d4d66b63ed679
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-10-02 17:18:36 +0530

    Refined dataset controller to remove similiar terms

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -3,28 +3,42 @@ class DatasetsController < ApplicationController
     headers['Access-Control-Allow-Origin'] = "*" 
     @datasets=Hash.new(0)
     @terms=Array.new()
-    @terms_related_to_requested_term=Hash.new
+    terms_related_to=Hash.new
 
-    @requested_terms=params[:terms].split(/[ -,.:;+]/)
-    @requested_terms.each do |requested_term|
-      @terms_related_to_requested_term[requested_term]=Array.new()
+    requested_terms=params[:terms].split(/[ -,.:;+]/)
+    requested_terms.each do |requested_term|
+      terms_related_to[requested_term]=Array.new()
       Term.find(:all, :conditions => ["lower(term) = ?", requested_term.downcase]).each do |term|
         term.term_items.each do |term_item|
           @datasets[term_item.dataset] += 1
           term_item.dataset.term_items.each do |ds_term|
-            @terms_related_to_requested_term[requested_term] << ds_term.term.term
+            term_value=ds_term.term.term
+            if !(terms_related_to[requested_term].include?(term_value) || terms_related_to[requested_term].include?(term_value.pluralize))
+              terms_related_to[requested_term].push(term_value)
+            end
           end
         end
       end  
       if(@terms.empty?)
-        @terms += @terms_related_to_requested_term[requested_term]
+        @terms += terms_related_to[requested_term]
       else
-        @terms = @terms & @terms_related_to_requested_term[requested_term]
+        @terms &= terms_related_to[requested_term]
       end
     end
 
     #Select top 25 datasets according to frequency of terms
     @datasets=@datasets.sort_by {|key,value| value}.reverse.first(25)
-    @terms=@terms.first(33)
+
+    terms_similar_to_requested_terms =  requested_terms
+    terms_similar_to_requested_terms += requested_terms.map  {|requested_term| requested_term.capitalize}
+    terms_similar_to_requested_terms += terms_similar_to_requested_terms.map  {|term| term.singularize}
+    terms_similar_to_requested_terms += terms_similar_to_requested_terms.map  {|term| term.pluralize}
+
+    terms_similar_to_requested_terms += requested_terms.map  {|requested_term| requested_term.upcase}
+    terms_similar_to_requested_terms.uniq!    
+
+    @terms -= terms_similar_to_requested_terms
+    
+    @terms = @terms.first(38).sort_by{ |m| m.match(/^[0-9]/) ? m : "A" }
   end
 end
